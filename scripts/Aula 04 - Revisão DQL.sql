991723a245699339bb3094bbbbdd054c552806c4
FonteneleGm <215144175+FonteneleGm@users.noreply.github.com>
2026-08-26 11:27:34 +0000

    "Aula 06"

--- scripts/Aula 04 - Revisão DQL.sql
+++ scripts/Aula 04 - Revisão DQL.sql
@@ -139,6 +139,99 @@ from funcionario f
 left join supervisor s
       on f.cpf_supervisor = s.cpf
 order by s.pnome null last, f.pnome, f.unome;
+-- Início Aula 06 26/08/2026
+select 
+      f.pnome || " " || f.unome funcionario,       
+      coalesce(s.pnome || " " || s.unome, 'Sem supervisor') supervisor
+from funcionario s
+right join funcionario f
+      on f.cpf_supervisor = s.cpf
+order by s.pnome null last, f.pnome, f.unome;
+
+-- Mudanças para visualizar FULL JOIN
+update funcionario
+set numero_departamento = null
+where cpf = '44455566677';
+
+insert into departamento(numero, nome, cpf_gerente, data_ini)
+values(4, 'Marketing', null, current_date)
+
+select 
+      coalesce(d.nome, 'Sem departamento') departamento,
+      coalesce(f.pnome || " " || f.unome, 'Sem funcionário')
+from departamento d
+full join funcionario f
+      on d.numero = f.numero_departamento
+order by departamento nulls last, funcionario nulls last;
 
 -- Exists, Not Exists
+--Listar funcionários que são gerentes de algum departamento
+select 
+      f.pnome || " " || f.unome funcionario
+from funcionario f
+where exists (
+      select *
+      from departamento d
+      where d.cpf_gerente = f.cpf
+)
+order by funcionario;
 
+-- Existe algum funcionário que não é gerente?
+select 
+      f.pnome || " " || f.unome funcionario
+from funcionario f
+where not exists (
+      select *
+      from departamento d
+      where d.cpf_gerente = f.cpf
+)
+order by funcionario;
+
+-- Funções de agrupamento: group by, having
+
+-- Qual o salário médio dos funcionários em cada departamento?
+select
+      numero_departamento, 
+      round(avg(salario), 2) media_salarial
+from funcionario
+group by numero_departamento
+order by numero_departamento;
+
+-- Qual o salário médio dos funcionários em cada departamento? (sem valores nulos) WHERE
+select
+      numero_departamento, 
+      round(avg(salario), 2) media_salarial
+from funcionario
+where numero_departamento is not null
+group by numero_departamento
+order by numero_departamento;
+
+-- Qual o salário médio dos funcionários em cada departamento? (sem valores nulos) HAVING
+select
+      numero_departamento, 
+      round(avg(salario), 2) media_salarial
+from funcionario
+group by numero_departamento
+having numero_departamento is not null
+order by numero_departamento;
+
+-- Qual o número de funcionários que trabalham em cada departamento?
+select
+      numero_departamento,
+      count(*)
+from funcionario f
+group by numero_departamento 
+order by numero_departamento;
+
+-- Listar número e nome do departamento, quantidade de funcionários, média salarial e folha salarial
+select
+      d.numero numero_departamento,
+      d.nome nome_departamento,
+      count(f.cpf) qtd_funcionarios,
+      round(avg(f.salario), 2) media_salarial,
+      sum(f.salario) folha salarial
+from funcionario f
+right join departamento d
+      on f.numero_departamento = d.numero
+group by numero_departamento
+order by numero_departamento;
